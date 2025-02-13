65b7c2e4f6aee536e9282308acb447e7b1e80050
Ubuntu <ubuntu@ip-172-31-8-223.ap-southeast-3.compute.internal>
2025-02-13 09:36:01 +0000

    first commit

---
+++ src/Domains/users/entities/_test/RegisterUser.test.js
@@ -0,0 +1,67 @@
+const RegisterUser = require('../RegisterUser');
+
+describe('a register entities', () => {
+  it('should throw error when payload did not contain needed property', () => {
+    // Arrange
+    const payload = {
+      username: 'abc',
+      password: 'abc',
+    };
+
+    // Action and Assert
+    expect(() => new RegisterUser(payload)).toThrowError('REGISTER_USER.NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should throw error when payload did not meet data type specification', () => {
+    // Arrange
+    const payload = {
+      username: 123,
+      password: true,
+      fullname: 'abc',
+    };
+
+    // Action and Assert
+    expect(() => new RegisterUser(payload)).toThrowError('REGISTER_USER.NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
+
+  it('should throw error when username contains more than 50 character', () => {
+    // Arange
+    const payload = {
+      username: 'a'.repeat(51),
+      password: 'abc',
+      fullname: 'abc',
+    };
+
+    // Action and Assert
+    expect(() => new RegisterUser(payload)).toThrowError('REGISTER_USER.USERNAME_LIMIT_CHAR');
+  });
+
+  it('should throw error when username contains restricted character', () => {
+    // Arrange
+    const payload = {
+      username: 'dico ding',
+      fullname: 'dicoding',
+      password: 'abc',
+    };
+
+    // Action and Assert
+    expect(() => new RegisterUser(payload)).toThrowError('REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER');
+  });
+
+  it('should create registeredUser object correctly', () => {
+    // Arrange
+    const payload = {
+      username: 'dicoding',
+      fullname: 'dicoding',
+      password: 'abc',
+    };
+
+    // Action
+    const { username, fullname, password } = new RegisterUser(payload);
+
+    // Assert
+    expect(username).toEqual(payload.username);
+    expect(fullname).toEqual(payload.fullname);
+    expect(password).toEqual(payload.password);
+  });
+});
